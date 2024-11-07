2a9e7691d5cb6f59c19abb442a57e7038b2dcb7c
Alonso Canales <aecanales@uc.cl>
2024-11-06 21:35:51 -0500

    ✨ Get builds working

---
+++ Assets/Scripts/GameBuildManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+[TypeInfoBox("Handles activating and de-activating the corresponding build game object depending on which games is being run.")]
+public class GameBuildManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameBuildManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    #endregion    
+
+    int enabledBuild = -1;
+    
+    [SerializeField] GameObject[] gamePrefabs;
+
+    public void EnableGameBuild(int build)
+    {
+        gamePrefabs[build].SetActive(true);
+
+        enabledBuild = build;
+    }
+
+    public void DisableGameBuild()
+    {
+        gamePrefabs[enabledBuild].SetActive(false);
+
+        enabledBuild = -1;
+    }
+}
