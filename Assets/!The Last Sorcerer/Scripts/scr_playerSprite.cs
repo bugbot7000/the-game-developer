593cfa4f5bf18863c143d87ebf73778ffee7c67f
Alonso Canales <aecanales@uc.cl>
2024-11-26 15:21:11 -0500

    🎨 Add walking animation

---
+++ Assets/!The Last Sorcerer/Scripts/scr_playerSprite.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class scr_playerSprite : MonoBehaviour
+{
+    scr_playerController player;
+    Animator animator;
+    bool flipX;
+
+    void Start()
+    {
+        player = FindFirstObjectByType<scr_playerController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position;        
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        animator.SetFloat("speed", 
+            new Vector3(horizontal, 0, vertical).magnitude
+        );
+
+        if (horizontal < 0 && !flipX) 
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            flipX = true;
+        }
+        else if (horizontal > 0 && flipX)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            flipX = false;
+        }
+    }
+}
