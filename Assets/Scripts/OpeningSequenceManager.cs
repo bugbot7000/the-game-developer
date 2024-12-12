8ea45c3fe798c23f76f63250f4ca64562eb02f49
Alonso Canales <aecanales@uc.cl>
2024-12-12 13:20:30 -0500

    📜 Add opening sequence

--- Assets/Scripts/OpeningSequenceManager.cs
+++ Assets/Scripts/OpeningSequenceManager.cs
@@ -1,13 +1,21 @@
+using System.Collections;
+
 using UnityEngine;
-using UnityEngine.UI;
 
 using DG.Tweening;
 
 public class OpeningSequenceManager : MonoBehaviour
 {
+    //ralia's lament should have some dialogue...
+    
     [SerializeField] CanvasGroup fade;
     [SerializeField] Canvas dreamOS;
     [SerializeField] Canvas titleScreen;
+    [SerializeField] GameObject raliasLamentBuild;
+    [SerializeField] RectTransform notifcation;
+    [SerializeField] RectTransform fullScreenRawImage;
+    [SerializeField] RectTransform mouseCursor;
+    [SerializeField] RectTransform topBar;
 
     void Start()
     {
@@ -21,10 +29,33 @@ public class OpeningSequenceManager : MonoBehaviour
             .Append(fade.DOFade(1.0f, 2.0f).SetEase(Ease.OutCubic))
             .AppendCallback(() => {
                 dreamOS.gameObject.SetActive(true);
+                raliasLamentBuild.SetActive(true);
                 titleScreen.gameObject.SetActive(false);
             })
-            .AppendInterval(1.0f)
+            .AppendInterval(2.0f)
             .Append(fade.DOFade(0.0f, 2.0f).SetEase(Ease.OutCubic))
-            .AppendCallback(() => fade.blocksRaycasts = false);
+            .AppendCallback(() => {
+                fade.blocksRaycasts = false;
+                notificationAndCloseGameSequence();
+            });
+
+        // instead of making it a build...
+        // then when we minimize it it goes to a window
+        // game should start with chat open, and steam open
+    }
+
+    void notificationAndCloseGameSequence()
+    {
+        DOTween.Sequence()
+            .AppendInterval(5.0f)
+            .Append(notifcation.DOAnchorPosX(-64, 0.5f).SetEase(Ease.OutCubic))
+            .AppendInterval(4.5f)
+            .Append(notifcation.DOAnchorPosX(400, 0.5f).SetEase(Ease.OutCubic))
+            .Join(mouseCursor.DOAnchorPosY(1025, 3.0f))
+            .Join(topBar.DOAnchorPosY(0, 0.5f))
+            .AppendInterval(1.5f)
+            .Append(fullScreenRawImage.DOScale(Vector3.zero, 1.0f).SetEase(Ease.InOutQuint))
+            .AppendCallback(() => topBar.gameObject.SetActive(false))
+            .AppendCallback(() => mouseCursor.gameObject.SetActive(false));
     }
 }
