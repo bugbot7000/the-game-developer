0e498f07021be8f7d540cfc0dc313bdb84ccc990
Alonso Canales <aecanales@uc.cl>
2025-01-23 15:36:11 -0500

    📜 Implement first pass at search function (no UI yet)

--- Assets/Scripts/Wiki/WikiPageSO.cs
+++ Assets/Scripts/Wiki/WikiPageSO.cs
@@ -21,6 +21,14 @@ public class WikiPageSO : ScriptableObject
     public bool ContainsBuild;
     [HorizontalGroup("Build"), ShowIf("@ContainsBuild")]
     public int BuildIndex;
-    [Tooltip("Allows for a page to be searched for by words that are not contained in title, subtitle, or content of the page.")]
+    [Tooltip("Allows for a page to be searched for by words that are not contained in title, subtitle, or content of the page. Keywords must be in lowercase!")]
     public List<string> AdditionalKeywords;
+
+    public bool PageContainsTerm(string term)
+    {
+        return Title.Contains(term, System.StringComparison.CurrentCultureIgnoreCase) || 
+            Subtitle.Contains(term, System.StringComparison.CurrentCultureIgnoreCase) || 
+            Content.Contains(term, System.StringComparison.CurrentCultureIgnoreCase)  || 
+            AdditionalKeywords.Contains(term.ToLower());
+    }
 }
