ddd39c87875f39da85a2f588a3f936abbbc8ae06
YaBoiPercy <iperseusftaylor@gmail.com>
2025-02-25 17:02:40 -0500

    shield first draft
    doesn't play nice with pull yet

--- Assets/!The Last Sorcerer/Scripts/scr_playerController.cs
+++ Assets/!The Last Sorcerer/Scripts/scr_playerController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class scr_playerController : MonoBehaviour
@@ -27,9 +28,10 @@ public class scr_playerController : MonoBehaviour
     public GameObject currentAttack;
     public GameObject charmedThrall;
     public GameObject[] currentFamiliars;
-    public GameObject swipe, beam;
+    public GameObject swipe, beam, shield;
     public bool spellOnCooldown;
     public float cooldownTime;
+    private Transform spellSpawn;
 
     public GameObject familiar1, familiar2, familiar3;
     public bool openFamiliarSlot = true;
@@ -85,8 +87,6 @@ public class scr_playerController : MonoBehaviour
             rotationSetting = new Vector3(0, 180f, 0f);
         }
     
-
-
         //if (Input.GetKeyDown(KeyCode.K))
         //{
         //    if (equippedSpell == spellA) { equippedSpell = spellB; }
@@ -141,7 +141,8 @@ public class scr_playerController : MonoBehaviour
             body.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
         }
 
-        //if (Input.GetKeyDown(KeyCode.I)) { SwitchSpell(equippedSpell); }
+        if (currentAttack != null && equippedSpell == shield) { gameObject.GetComponent<scr_health>().invincible = true; }
+        else { gameObject.GetComponent<scr_health>().invincible = false; }
         if (health <= 0f) { Death(); }
     }
 
@@ -179,6 +180,11 @@ public class scr_playerController : MonoBehaviour
         spell1 = beam;
     }
 
+    public void SwitchSpell1ToShield()
+    {
+        spell1 = shield;
+    }
+
     public void SwitchSpell2ToSwipe()
     {
         spell2 = swipe;
@@ -186,6 +192,10 @@ public class scr_playerController : MonoBehaviour
     public void SwitchSpell2ToBeam()
     {
         spell2 = beam;
+    }
+    public void SwitchSpell2ToShield()
+    {
+        spell2 = shield;
     }
 
     public void SlotMyFamiliar(GameObject familiar)
@@ -259,7 +269,15 @@ public class scr_playerController : MonoBehaviour
     {
         if (currentAttack == null)
         {
-            var copy = Instantiate(castSpell, activeFirePoint.transform.position, Quaternion.identity);
+            if (castSpell == shield)
+            {
+                spellSpawn = gameObject.transform;
+            }
+            else
+            {
+                spellSpawn = activeFirePoint.transform;
+            }
+            var copy = Instantiate(castSpell, spellSpawn.position, Quaternion.identity);
             copy.transform.eulerAngles = rotationSetting;
             currentAttack = copy;
             if (type == SpellType.Push)
