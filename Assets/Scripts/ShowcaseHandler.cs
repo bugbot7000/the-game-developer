21849c5e1caf8c429ff6c01ad90e8f89b3e33696
Alonso Canales <aecanales@uc.cl>
2025-03-15 01:05:19 -0400

    📦 Import showcase handler (but it doesnt work)

---
+++ Assets/Scripts/ShowcaseHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowcaseHandler : MonoBehaviour
+{
+    public int IdleTimeSetting = 5;
+    public float IdleTime;
+    public float idleInSeconds;
+    public GameObject IdleAnimator;
+    
+    private bool isRestarting = false;
+    
+    void Awake()
+    {
+        idleInSeconds = IdleTimeSetting * 60;
+    }
+        
+    private void Update(){
+        if(isRestarting && Input.anyKey){
+            isRestarting = false;
+            IdleAnimator.gameObject.SetActive(false);
+        }
+
+        if(Input.anyKey) {
+            IdleTime = 0;
+        }
+
+        if (IdleTime >= idleInSeconds-4) {
+            isRestarting = true;
+            IdleAnimator.gameObject.SetActive(true);  
+        } else {
+            IdleCheck();
+        }
+        
+        if(Input.GetKeyUp(KeyCode.Backslash)){
+            IdleAnimator.gameObject.SetActive(true);
+            isRestarting = true;
+        }
+        
+    }
+        
+    public void IdleCheck(){
+        IdleTime += Time.deltaTime;
+    }
+}
