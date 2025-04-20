6d72b2692961e6ab7e1d447f8df275e24cc0710d
Aman Kapoor <32449543+96aman96@users.noreply.github.com>
2025-04-20 19:20:40 -0400

    Spell selector UI (#3)
    * Add Healthbar and Spell Selector
    
    Healthbar can now be controlled by calling relevant methods
    Stat Selector is fully functional
    
    * Prepare branch for merge
    
    Clean up asset folder, create prefabs

---
+++ Assets/Scripts/IsAbilitySelected.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class IsAbilitySelected : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool isSelected;
+
+    public void Played()
+    {
+        Debug.Log("Played");
+    }
+
+    public void Stopped()
+    {
+        Debug.Log("Stopre");
+    }
+    
+}
+
+
