c8ebfa07602add05d6677b19b2bb42024b93cfa1
Alonso Canales <aecanales@uc.cl>
2025-04-22 09:59:24 -0400

    🎬 Remove title screen from desktop scene

--- Assets/Scripts/OpeningSequenceManager.cs
+++ Assets/Scripts/OpeningSequenceManager.cs
@@ -12,7 +12,6 @@ public class OpeningSequenceManager : MonoBehaviour
     [SerializeField] bool skipOpening;
     [SerializeField] CanvasGroup fade;
     [SerializeField] Canvas dreamOS;
-    [SerializeField] Canvas titleScreen;
     [SerializeField] GameObject raliasLamentBuild;
     [SerializeField] RectTransform notifcation;
     [SerializeField] RectTransform fullScreenRawImage;
@@ -30,13 +29,12 @@ public class OpeningSequenceManager : MonoBehaviour
             Debug.Log("[OpeningSequenceManager] Skipping opening sequence.");
 
             dreamOS.gameObject.SetActive(true);
-            titleScreen.gameObject.SetActive(false);
             fade.gameObject.SetActive(false);
             fullScreenRawImage.parent.gameObject.SetActive(false);
         }
         else
         {
-            fade.DOFade(0.0f, 2.0f).SetEase(Ease.OutCubic).OnComplete(() => fade.blocksRaycasts = false);
+            StartGame();
         }
     }
 
@@ -44,16 +42,14 @@ public class OpeningSequenceManager : MonoBehaviour
     {
         fade.blocksRaycasts = true;
         DOTween.Sequence()
-            .Append(fade.DOFade(1.0f, 2.0f).SetEase(Ease.OutCubic))
             .AppendCallback(() => {
                 dreamOS.gameObject.SetActive(true);
                 raliasLamentBuild.SetActive(true);
-                titleScreen.gameObject.SetActive(false);
                 messaging.OpenWindow();
                 // webBrowser.OpenWindow();
                 // webBrowser.GetComponent<WebBrowserManager>().OpenPage("www.vapor.com/ralias-lament");
             })
-            .AppendInterval(2.0f)
+            .AppendInterval(3.0f)
             .Append(fade.DOFade(0.0f, 2.0f).SetEase(Ease.OutCubic))
             .AppendCallback(() => {
                 fade.blocksRaycasts = false;
@@ -68,12 +64,15 @@ public class OpeningSequenceManager : MonoBehaviour
             .Append(notifcation.DOAnchorPosX(-64, 0.5f).SetEase(Ease.OutCubic))
             .AppendInterval(4.5f)
             .Append(notifcation.DOAnchorPosX(400, 0.5f).SetEase(Ease.OutCubic))
-            .Join(mouseCursor.DOAnchorPosY(1025, 3.0f))
-            .Join(topBar.DOAnchorPosY(0, 0.5f))
-            .AppendInterval(1.5f)
-            .Append(fullScreenRawImage.DOScale(Vector3.zero, 1.0f).SetEase(Ease.InOutQuint))
-            .AppendCallback(() => topBar.gameObject.SetActive(false))
-            .AppendCallback(() => mouseCursor.gameObject.SetActive(false))
-            .AppendCallback(() => raliasLamentBuild.SetActive(false));
+            .Join(topBar.DOAnchorPosY(0, 0.5f));
+    }
+
+    public void CloseGame()
+    {
+        fullScreenRawImage.DOScale(Vector3.zero, 1.0f).SetEase(Ease.InOutQuint).OnComplete( () => {
+            topBar.gameObject.SetActive(false);
+            mouseCursor.gameObject.SetActive(false);
+            raliasLamentBuild.SetActive(false);
+        });
     }
 }
